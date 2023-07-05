90a1bf0fbffa7a7772aad24c9958ddf3405bef25
Paloma <kleber@gmail.com>
2023-07-05 16:05:03 -0300

    primeiro commit

---
+++ script.js
@@ -0,0 +1,18 @@
+alert ("A página foi carregada com sucesso")
+
+let x=10;
+var y=30;
+
+console.log (x+y)
+document.write('A soma é = ' + (x+y))
+
+console.log (x/y)
+document.write('A divisão é = ' + (x/y))
+
+console.log (x*y)
+document.write ('A multiplicação é = ' + (x*y))
+
+console.log(x>=y);
+console.log(x == 10);
+
+
